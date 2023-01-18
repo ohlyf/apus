c1e70467923b2e53e562eb991e9a550dfc193b7d
李云富 <liyunfu@liyunfus-MacBook-Pro.local>
2023-01-18 16:03:36 +0800

    chore: 接入vitest单元测试工具

---
+++ src/node/__test__/index.test.ts
@@ -0,0 +1,8 @@
+import exp from 'constants';
+import { expect, test } from 'vitest';
+
+test('add', () => {
+  expect(1 + 1).toBe(2);
+  expect('map'.slice(1)).toMatchSnapshot('"ap"');
+  expect('map'.slice(1)).toMatchInlineSnapshot('"ap"');
+});
