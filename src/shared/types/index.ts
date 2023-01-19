97daa7281b61c9e8aad0355d32bae6f04010b18f
李云富 <liyunfu@liyunfus-MacBook-Pro.local>
2023-01-19 10:51:46 +0800

    feat: 添加通用配置解析器

---
+++ src/shared/types/index.ts
@@ -0,0 +1,36 @@
+import { UserConfig as ViteConfiguration } from 'vite';
+
+export interface NavItemWithLink {
+  text: string;
+  link: string;
+}
+
+export interface SidebarGroup {
+  text: string;
+  items: SidebarItem[];
+}
+
+export interface Footer {
+  message: string;
+}
+
+export interface SidebarItem {
+  text?: string;
+  link?: string;
+  footer?: Footer;
+}
+
+export interface Sidebar {
+  [path: string]: SidebarGroup[];
+}
+export interface ThemeConfig {
+  nav?: NavItemWithLink[];
+  sidebar?: Sidebar;
+}
+
+export interface UserConfig {
+  title: string;
+  description: string;
+  themeConfig: ThemeConfig;
+  vite: ViteConfiguration;
+}
